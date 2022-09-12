c9f6317e7662427b34af794b03689d49ff3d4082
hasan <hasan.wbdv@gmail.com>
2022-09-12 17:17:44 +0430

    fix:view some bugs

--- resources/views/professors/create.blade.php
+++ resources/views/professors/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Create Professor - MDITech') }}
+            {{ __('Create Professor - Auto Apply') }}
         </h2>
     </x-slot>
     <div class="py-12">
